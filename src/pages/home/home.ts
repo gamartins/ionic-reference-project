b06f8f457bb332075eb3f1cab638aa0d52bdf929
Gabriel Angelo <gabriel.angelo91@gmail.com>
2018-01-04 21:28:20 -0300

    feat: logic for forms using FormBuilder

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+
+import { ErrorChecker } from '../../util/error-checker'
+import { CustomValidators } from '../../util/custom-validators'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private someForm : FormGroup;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public toastCtrl: ToastController,
+    private formBuilder: FormBuilder) {
+    
+    this.someForm = this.formBuilder.group({
+      name: [ '', Validators.required ],
+      email: [ '', Validators.compose([ Validators.required, Validators.email ])],
+      emailConfirmation: [ '', Validators.compose([ Validators.required, Validators.email ])],
+      password: [ '', Validators.required ],
+      passwordConfirmation: [ '', Validators.required ],
+      useTerms: [ false , Validators.requiredTrue ]
+    }, {
+      validator: [ CustomValidators.matchPassword, CustomValidators.matchEmail]
+    })
 
   }
 
+  showMessage(message) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    }).present()
+  }
+
+  send(){
+    const errors = ErrorChecker.getFormError(this.someForm.controls)
+    
+    if(errors.length == 0)
+      console.log(this.someForm.value)
+    else
+      this.showMessage(errors[0])
+  }
 }
---
+++ src/util/error-checker.ts
@@ -0,0 +1,38 @@
+export class ErrorChecker {
+    public static getFormError(controls) {
+        let errors = []
+
+        Object.keys(controls).forEach(key => {
+            if(controls[key].errors)
+                console.log(controls[key].errors)
+
+            if(controls[key].errors && controls[key].errors.required)
+                errors.push(`O campo ${key} é obrigatório`)
+            
+            if(controls[key].errors && controls[key].errors.minlength) {
+                const requiredLength = controls[key].errors.minlength.requiredLength
+                errors.push(`O campo ${key} deve possuir no mínimo ${requiredLength } caracteres`)
+            }
+
+            if(controls[key].errors && controls[key].errors.maxlength) {
+                const requiredLength = controls[key].errors.maxlength.requiredLength
+                errors.push(`O campo ${key} deve possuir no máximo ${requiredLength} caracteres`)
+            }
+
+            if(controls[key].errors && controls[key].errors.email) {
+                errors.push(`O campo ${key} deve estar no formato nome@email.com`)
+            }
+
+            if(controls[key].errors && controls[key].errors.matchPassword) {
+                errors.push(`O campos senha e confirmar senha devem ser iguais`)
+            }
+
+            if(controls[key].errors && controls[key].errors.matchEmail) {
+                errors.push(`O campos email e confirmar email devem ser iguais`)
+            }
+        })
+
+        return errors
+
+    }
+}
